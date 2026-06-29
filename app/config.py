a4dc14c5ac1b5d00bdf9016417f033540238f122
Gerrit Watermann <g.watermann2@gmx.de>
2026-06-29 15:40:45 +0200

    fix(processing): formatted

--- app/config.py
+++ app/config.py
@@ -1,11 +1,8 @@
 import os
-from dataclasses import dataclass
 from pathlib import Path
 
 DATA_DIR_ENV_VAR = "AUDIOEXPLORER_DATA_DIR"
 
 
-
 def get_data_dir() -> Path:
     return Path(os.environ[DATA_DIR_ENV_VAR])
-
